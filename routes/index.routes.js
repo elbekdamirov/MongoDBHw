937ff52c6f3397bd0336db184e84ca1b510bbc64
elbekdamirov <elbekmain@gmail.com>
2025-05-06 09:57:02 +0500

    index routes

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
-const clientRouter = require("./client.routes")
+const clientRouter = require("./client.routes");
+const adminRouter = require("./admin.routes");
+const currencyRouter = require("./currency.routes");
+const operationRouter = require("./operation.routes");
+const oderRouter = require("./order.routes");
+const statusRouter = require("./status.routes");
 
 let router = Router();
 
-
-router.use("/clients", clientRouter)
+router.use("/clients", clientRouter);
+router.use("/admins", adminRouter);
+router.use("/currency", currencyRouter);
+router.use("/clients", clientRouter);
+router.use("/operation", operationRouter);
+router.use("/order", oderRouter);
+router.use("/status", statusRouter);
 
 module.exports = router;
